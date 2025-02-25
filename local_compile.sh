05cfc45a396c405aba51958289b91b185355e5dc
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-02-25 15:21:26 +0100

    build

--- local_compile.sh
+++ local_compile.sh
@@ -7,16 +7,27 @@ lib_name=${pkg_name}${ext_suffix}
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   shared_library_ext=.so
+  shared_library_header=lib
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   shared_library_ext=.dylib
+  shared_library_header=lib
 else
-  shared_library_ext=.dll
+  shared_library_ext=.lib
+  shared_library_header=
 fi
+
+build() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "darwin"* ]]; then
+    make -j
+  else
+    cmake --build . --config Release
+  fi
+}
 
 cython --cplus -3 $pkg_name.pyx -o $pkg_name.cc
 
 mkdir -p build
 cd build
 cmake -DEXT_NAME=$lib_name -DCYTHON_CPP_FILE=$pkg_name.cc ..
-make -j
-mv lib$lib_name$shared_library_ext ../$lib_name
+build
+mv $shared_library_header$lib_name$shared_library_ext ../$lib_name
