edbe4b22f69bdcba6271f73920ec5e24eb8127fd
Gaëtan Serré <56162277+gaetanserre@users.noreply.github.com>
2025-02-26 16:03:44 +0100

    Add Windows compilation (#1)

--- local_compile.sh
+++ local_compile.sh
@@ -4,6 +4,7 @@ pkg_name=gkls
 ext_suffix=$(python -c "from importlib.machinery import EXTENSION_SUFFIXES; print(EXTENSION_SUFFIXES[0])")
 
 lib_name=${pkg_name}${ext_suffix}
+shared_library_header=lib
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   shared_library_ext=.so
@@ -11,12 +12,22 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   shared_library_ext=.dylib
 else
   shared_library_ext=.dll
+  shared_library_header=Release/
 fi
+
+build() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "darwin"* ]]; then
+    cmake -DEXT_NAME=$lib_name -DCYTHON_CPP_FILE=$pkg_name.cc ..
+    make -j
+  else
+    cmake -DEXT_NAME=$lib_name -DCYTHON_CPP_FILE=$pkg_name.cc -G "Visual Studio 17 2022" -A x64 ..
+    cmake --build . --config Release
+  fi
+}
 
 cython --cplus -3 $pkg_name.pyx -o $pkg_name.cc
 
 mkdir -p build
 cd build
-cmake -DEXT_NAME=$lib_name -DCYTHON_CPP_FILE=$pkg_name.cc ..
-make -j
-mv lib$lib_name$shared_library_ext ../$lib_name
+build
+mv $shared_library_header$lib_name$shared_library_ext ../$lib_name
